d913e959dd744dc012a25b5eadcc4b78c8ba2a78
phani2585 <phani2585@gmail.com>
2019-05-15 20:22:06 +0530

    Added 'Details' component which displays the details page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Details from '../details/Details';
 import './Home.css';
 import Header from '../../common/header/Header';
 import { withStyles } from '@material-ui/core/styles';
@@ -19,6 +21,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import genres from '../../common/genres';
 import artists from '../../common/artists';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
     root: {
@@ -72,6 +75,10 @@ class Home extends Component {
         this.setState({ artists: event.target.value });
     }
 
+    movieClickHandler = (movieId) => {
+        ReactDOM.render(<Details movieId={movieId} />, document.getElementById('root'));
+    }
+
     render() {
         const { classes } = this.props;
         return (
@@ -94,7 +101,7 @@ class Home extends Component {
                     <div className="left">
                         <GridList cellHeight={350} cols={4} className={classes.gridListMain}>
                             {moviesData.map(movie => (
-                                <GridListTile className="released-movie-grid-item" key={"grid" + movie.id}>
+                                <GridListTile className="released-movie-grid-item" onClick={() => this.movieClickHandler(movie.id)} key={"grid" + movie.id}>
                                     <img src={movie.poster_url} className="movie-poster" alt={movie.title} />
                                     <GridListTileBar
                                         title={movie.title}
@@ -176,6 +183,12 @@ class Home extends Component {
                                         InputLabelProps={{ shrink: true }}
                                     />
                                 </FormControl>
+
+                                <div className={classes.formControl}>
+                        <Button variant="contained" color="primary" onClick={this.openModalHandler}>
+                            APPLY
+                        </Button>
+                    </div>
 
                             </CardContent>
                         </Card>
