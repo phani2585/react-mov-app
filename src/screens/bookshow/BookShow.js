38da7eeb929a429a0bf8dacf33587e25c0ef46e1
phani2585 <phani2585@gmail.com>
2019-05-15 22:04:49 +0530

    Added required field validation on book show page

--- src/screens/bookshow/BookShow.js
+++ src/screens/bookshow/BookShow.js
@@ -16,6 +16,7 @@ import Input from '@material-ui/core/Input';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
 
 
@@ -30,7 +31,12 @@ import Button from '@material-ui/core/Button';
             showTime: "",
             tickets: 0,
             unitPrice: 500,
-            availableTickets: 20
+            availableTickets: 20,
+            reqLocation: "dispNone",
+            reqLanguage: "dispNone",
+            reqShowDate: "dispNone",
+            reqShowTime: "dispNone",
+            reqTickets: "dispNone"
         }
     }
 
@@ -53,6 +59,14 @@ import Button from '@material-ui/core/Button';
     }
     ticketsChangeHandler = (event) => {
         this.setState({ tickets: event.target.value })
+    }
+
+    bookShowButtonHandler = () => {
+        this.state.location === "" ? this.setState({ reqLocation: "dispBlock" }) : this.setState({ reqLocation: "dispNone" });
+        this.state.language === "" ? this.setState({ reqLanguage: "dispBlock" }) : this.setState({ reqLanguage: "dispNone" });
+        this.state.showDate === "" ? this.setState({ reqShowDate: "dispBlock" }) : this.setState({ reqShowDate: "dispNone" });
+        this.state.showTime === "" ? this.setState({ reqShowTime: "dispBlock" }) : this.setState({ reqShowTime: "dispNone" });
+        this.state.tickets === 0 ? this.setState({ reqTickets: "dispBlock" }) : this.setState({ reqTickets: "dispNone" });
     }
     render() {
         return (
@@ -81,6 +95,9 @@ import Button from '@material-ui/core/Button';
                                         </MenuItem>
                                     ))}
                                 </Select>
+                                <FormHelperText className={this.state.reqLocation}>
+                                    <span className="red">Required</span>
+                                </FormHelperText>
                             </FormControl>
                             <br /><br />
                             <FormControl required className="formControl">
@@ -95,6 +112,9 @@ import Button from '@material-ui/core/Button';
                                         </MenuItem>
                                     ))}
                                 </Select>
+                                <FormHelperText className={this.state.reqLanguage}>
+                                    <span className="red">Required</span>
+                                </FormHelperText>
                             </FormControl>
                             <br /><br />
                             <FormControl required className="formControl">
@@ -109,6 +129,9 @@ import Button from '@material-ui/core/Button';
                                         </MenuItem>
                                     ))}
                                 </Select>
+                                <FormHelperText className={this.state.reqShowDate}>
+                                    <span className="red">Required</span>
+                                </FormHelperText>
                             </FormControl>
                             <br /><br />
                             <FormControl required className="formControl">
@@ -123,11 +146,17 @@ import Button from '@material-ui/core/Button';
                                         </MenuItem>
                                     ))}
                                 </Select>
+                                <FormHelperText className={this.state.reqShowTime}>
+                                    <span className="red">Required</span>
+                                </FormHelperText>
                             </FormControl>
                             <br /><br />
                             <FormControl required className="formControl">
                                 <InputLabel htmlFor="tickets">Tickets: ( {this.state.availableTickets} available )</InputLabel>
                                 <Input id="tickets" value={this.state.tickets !== 0 ? this.state.tickets : ""} onChange={this.ticketsChangeHandler} />
+                                <FormHelperText className={this.state.reqTickets}>
+                                    <span className="red">Required</span>
+                                </FormHelperText>
                             </FormControl>
                             <br /><br />
                             <Typography>
